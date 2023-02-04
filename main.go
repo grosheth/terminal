14f3d3fb522d97c489b70bd5c6655cd4b6f5e4c9
Salledelavage <salledelavager@gmail.com>
2023-02-04 15:58:39 -0500

    save

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
+/*
+Copyright © 2023 NAME HERE <salledelavager@gmail.com>
+*/
 package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"os/user"
+	"time"
+
+	"github.com/grosheth/terminal/cmd"
 )
 
 func main() {
-	app := "echo"
+	cmd.Execute()
+	for {
+		new_line()
+		var cmd string
+		fmt.Scan(&cmd)
+		exec_command(cmd)
+		fmt.Println("You typed in:", cmd)
+	}
 
-	arg0 := "-e"
-	arg1 := "Hello world"
-	arg2 := "\n\tfrom"
-	arg3 := "golang"
+}
 
-	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
-	stdout, err := cmd.Output()
+func new_line() {
+
+	username, _ := user.Current()
+	hostname, _ := os.Hostname()
+	dt := time.Now()
+
+	fmt.Println("-__>", username.Username, "@", hostname, dt.Format("15:04:05"))
+
+}
+
+func exec_command(cmd string) {
+
+	command := exec.Command(cmd)
+	stdout, err := command.Output()
 
 	if err != nil {
 		fmt.Println(err.Error())

aa21115b7e66418487f05050bdb14ee14e16bcc5
Salledelavage <salledelavager@gmail.com>
2023-02-12 10:21:20 -0500

    import

--- cmd/git/init.go
+++ cmd/git/init.go
@@ -12,13 +12,8 @@ import (
 // initCmd represents the init command
 var InitCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Initialise the current repo and create the repo on git",
+	Long:  `Initialise the current repo and create the repo on git`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
